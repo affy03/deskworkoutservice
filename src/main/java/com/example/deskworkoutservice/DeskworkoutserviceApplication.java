258aa7d68db4a2cb31c4169b0e1cf264369ba2fb
progra33erJ <3affy.happy7@gmail.com>
2024-06-03 11:29:14 +0900

    プロジェクトのセットアップ

---
+++ src/main/java/com/example/deskworkoutservice/DeskworkoutserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.deskworkoutservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskworkoutserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeskworkoutserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/deskworkoutservice/DeskworkoutserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deskworkoutservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeskworkoutserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
