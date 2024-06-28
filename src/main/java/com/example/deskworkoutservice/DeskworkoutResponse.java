50684fc11136871d81f5fb3959240545f6a18b17
progra33erJ <3affy.happy7@gmail.com>
2024-06-28 09:38:26 +0900

    Responseクラスのsetter削除

--- src/main/java/com/example/deskworkoutservice/DeskworkoutResponse.java
+++ src/main/java/com/example/deskworkoutservice/DeskworkoutResponse.java
@@ -10,8 +10,4 @@ public class DeskworkoutResponse {
     public String getMessage() {
         return message;
     }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
